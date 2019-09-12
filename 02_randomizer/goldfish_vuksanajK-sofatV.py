62978598fc1f81b838f9063c8925e9e78a603250
kiran-vuksanaj <mvuksanaj00@stuy.edu>
2019-09-12 08:37:23 -0400

    moved file, fixed KREWES

---
+++ 02_randomizer/goldfish_vuksanajK-sofatV.py
@@ -0,0 +1,25 @@
+import random
+# static: (the crew names)
+KREWES = {
+
+        'orpheus': ['Emily', 'Kevin', 'Vishwaa', 'Eric', 'ray', 'Jesse', 'Tiffany', 'Amanda', 'Junhee', 'Jackie ', 'Tyler', 'Emory', 'Ivan', 'Elizabeth', 'Pratham', 'Shaw', 'Eric', 'Yaru', 'Kelvin', 'Hong Wei', 'Michael', 'Kiran', 'Amanda', 'Joseph', 'Tanzim', 'David', 'Yevgeniy'],
+
+        'rex': ['William', 'Joseph', 'Calvin', 'Ethan', 'Moody', 'Mo', 'Big Mo', 'Peihua', 'Saad', 'Benjamin', 'Justin', 'Alice', 'Hilary', 'Ayham', 'Michael', 'Matthew', 'Jionghao', 'Devin ', 'David', 'Jacob', 'Will', 'Hannah', 'Alex'],
+
+        'endymion': ['Grace', 'Nahi', 'Derek', 'Jun Tao', 'Connor', 'Jason', 'Tammy', 'Albert', 'Kazi', 'Derek', 'Brandon', 'Kenneth', 'Lauren', 'Biraj', 'Jeff', 'Jackson', 'Taejoon', 'Kevin', 'Jude', 'Sophie', 'Henry', 'Coby', 'Manfred', 'Leia', 'Ahmed', 'Winston']
+
+}
+
+def getName(team):
+    # precondition: String team is one of the team names (i.e orpheus, rex, endymion)
+    if not(team in KREWES.keys()):
+        return '' # to avoid a program stopping error
+    return random.choice(KREWES[team])
+
+# TEST CASES
+
+print( getName("orpheus") )
+print( getName("endymion") )
+print( getName("rex") )
+print( getName("this is not a team name"))
+print( getName("orpheus") )
