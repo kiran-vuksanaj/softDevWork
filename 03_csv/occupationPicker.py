2a0af0620d8979a215e10a5bb00f74bdb862b629
kiran-vuksanaj <mvuksanaj00@stuy.edu>
2019-09-17 07:00:04 -0400

    random job chooser

--- 03_csv/occupationPicker.py
+++ 03_csv/occupationPicker.py
@@ -17,12 +17,15 @@ with open('occupations.csv') as csv_file:
         else:
             dict[row[0]] = float(row[1]) / 100
 
-print dict
+# print dict
 
 def randomOccupation(dict):
-    randomVal = random.random()
+    randomVal = random.random() # random 0 to 1
+    percentageTracker = 0.0;
     for key in dict:
-        print key
-        print dict[key]
+        # adds each percentage to a running total, meaning that theres a (percentage) chance the random value is newly underneath the total
+        percentageTracker = percentageTracker + dict[key]
+        if randomVal < percentageTracker:
+            return key
 
-randomOccupation(dict)
+print randomOccupation(dict)
