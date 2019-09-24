c342554d23b1bda7679b32ab4c3110d0781183e4
kiran-vuksanaj <mvuksanaj00@stuy.edu>
2019-09-24 05:03:34 -0400

    copy of code from morty's repo, reflecting in class work again

--- 10_occ/app.py
+++ 10_occ/app.py
@@ -6,14 +6,12 @@ with open('occupations.csv') as csv_file:
     #code by Vishwaa, edited by kiran
     read = csv.reader(csv_file, delimiter = ",")
     dict = {}
-    l = 0
+    displayData = {}
+    print(read)
     for row in read:
-        # print row
-        if l == 0:
-            # skip header row
-            l = l + 1
-        else:
-            dict[row[0]] = float(row[1]) / 100
+        if not (row[0] == "Job Class" or row[0] == "Total"):
+            dict[row[0]] = float(row[1])
+        displayData[row[0]] = row[1]
 
 #WEIGHTED RANDOM CHOICE (mortiestRick)
 
@@ -36,3 +34,14 @@ def hi():
     return "moustache"
 
 @app.route("/occupyflaskst")
+def occupy():
+    return render_template(
+        "flaskStTemplate.html",
+        dict=displayData,
+        randomJob=weightedRandFromDict(dict)
+    )
+
+
+if(__name__ == "__main__"):
+    app.debug = True
+    app.run()
