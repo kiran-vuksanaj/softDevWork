53f6942cf2e217a4eb6035505098af60c602eb8c
kiran-vuksanaj <mvuksanaj00@stuy.edu>
2019-10-04 08:46:56 -0400

    files copied from alnassera

--- dolphinsGrace/app.py
+++ dolphinsGrace/app.py
@@ -1,5 +1,10 @@
+# Kiran Vuksanaj
+# SoftDev1 pd1
+# K12 -- ECHO Echo echo
+# 2019-09-26
+
 from flask import Flask, render_template, request
-import cgi
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -10,9 +15,19 @@ def hello_world():
 
 @app.route("/auth")
 def authPage():
-    req = request.args["words"]
+    username = request.args["username"]
+    request_method = request.method
+    #print(request)
+    #print(request.args)
     #ImmutableMultiDict([('words', u'woot!'), ('submit', u'Submit')])
-    return "it seems to me this has worked! Your word: " + req
+    #print(request.method)
+    #GET
+    #print("hello_world")
+    #print(request.headers)
+    return render_template("response.html",
+                            username=username,
+                            request_method=request_method
+                            )
 
 if __name__ == "__main__":
     app.debug = True
