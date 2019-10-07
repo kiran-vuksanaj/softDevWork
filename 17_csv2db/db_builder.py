5872b7449740dbcfd469306baee872df6d891c2b
kiran-vuksanaj <mvuksanaj00@stuy.edu>
2019-10-07 08:33:43 -0400

    care package unpacked

---
+++ 17_csv2db/db_builder.py
@@ -0,0 +1,28 @@
+#Kiran Vusksanaj
+#SoftDev pd1
+#skeleton :: SQLITE3 BASICS
+#07 Oct 2019
+
+# initialized from care package
+
+import sqlite3   #enable control of an sqlite database
+import csv       #facilitate CSV I/O
+
+
+DB_FILE="discobandit.db"
+
+db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+c = db.cursor()               #facilitate db ops
+
+#==========================================================
+
+# < < < INSERT YOUR POPULATE-THE-DB CODE HERE > > >
+
+
+command = ""          # test SQL stmt in sqlite3 shell, save as string
+c.execute(command)    # run SQL statement
+
+#==========================================================
+
+db.commit() #save changes
+db.close()  #close database
