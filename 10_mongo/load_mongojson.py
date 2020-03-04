16ffa6070617b05924758f9c0458ce4cc7669b5c
Kiran Vuksanaj <kiran.vuksanaj@gmail.com>
2020-03-04 11:32:28 +0000

    touchups on the loading, space for pruning if necessary

--- 10_mongo/load_mongojson.py
+++ 10_mongo/load_mongojson.py
@@ -9,17 +9,20 @@ from urllib.request import Request, urlopen
 
 DEFAULT_FILENAME = "movies.json"
 
-def load_mongo_json(filename,db):
+def load_mongo_json(filename,coll):
     with open(filename,"r") as jsonfile:
         stringcontent = jsonfile.read()
         content = loads(stringcontent)
-        db.insert(content)
+        coll.insert(content)
 
+def prune_data(coll):
+    pass
+        
 if __name__ == "__main__":
     client = MongoClient()
-    db = client.test
-    TobyTop40 = db.TobyTop40
-    load_mongo_json(DEFAULT_FILENAME,TobyTop40)
-    print("complete!")
+    db_tt4 = client.TobyTop40
+    coll_movies = db_tt4.movies
+    load_mongo_json(DEFAULT_FILENAME,coll_movies)
+    print("data successfully loaded\nlook in database 'TobyTop40', collection 'movies'")
 
 
