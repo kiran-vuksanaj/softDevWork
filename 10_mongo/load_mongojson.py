cbb8276b7924dbfdd594b08d9566c3525838e504
Kiran Vuksanaj <kiran.vuksanaj@gmail.com>
2020-03-04 11:26:00 +0000

    successful load but its in the test database

--- 10_mongo/load_mongojson.py
+++ 10_mongo/load_mongojson.py
@@ -7,18 +7,19 @@ from json import loads
 from pymongo import MongoClient
 from urllib.request import Request, urlopen
 
-DEFAULT_URL = "https://raw.githubusercontent.com/prust/wikipedia-movie-data/master/movies.json"
+DEFAULT_FILENAME = "movies.json"
 
-def load_mongo_json(url,db):
-    req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-    response = loads(urlopen(req).read().decode('utf-8'))
-    db.insert(response)
+def load_mongo_json(filename,db):
+    with open(filename,"r") as jsonfile:
+        stringcontent = jsonfile.read()
+        content = loads(stringcontent)
+        db.insert(content)
 
 if __name__ == "__main__":
     client = MongoClient()
     db = client.test
     TobyTop40 = db.TobyTop40
-    load_mongo_json(DEFAULT_URL,TobyTop40)
+    load_mongo_json(DEFAULT_FILENAME,TobyTop40)
     print("complete!")
 
 
