d3232fc0edb2c59248650ce0e1c5f097b372a611
Kiran Vuksanaj <kiran.vuksanaj@gmail.com>
2020-03-04 23:24:09 +0000

    file for kevin bacon number stuff

---
+++ 10_mongo/bacon.py
@@ -0,0 +1,17 @@
+# Kiran Vuksanaj & Connor Oh - TonyTop40
+# Softdev2 pd9
+# K10_xtra - kevin bacon number
+# 2020-03-04
+
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client.TobyTop40
+movie_coll = db.movies
+actor_coll = db.performers
+
+def costars(name):
+    
+
+def gen_actornumber(name):
+    pass
